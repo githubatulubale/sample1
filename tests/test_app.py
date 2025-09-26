7f41848a58776a812735b72197d8033c79638b3b
atulubale <atulubale347@gmail.com>
2025-09-26 18:42:19 +0530

    added all files

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+from src.app import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(3, 5) == -2
